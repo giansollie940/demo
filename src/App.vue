094466250d09893a21a83bc4e87c4a0083711ba2
giansollie940 <giansollie940@gmail.com>
2026-09-24 01:31:43 +0700

    dele

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import AppDialogHost from './components/ui/AppDialogHost.vue'
-import { useRealtimeInvalidation } from './realtime/useRealtimeInvalidation'
-
-useRealtimeInvalidation()
-</script>
-<template><RouterView /><AppDialogHost /></template>
--- src/features/profile/gif-loop.d.ts
+++
@@ -1,1 +0,0 @@
-export function ensureGifInfiniteLoop(input: Uint8Array<ArrayBuffer> | ArrayBuffer): Uint8Array<ArrayBuffer>
